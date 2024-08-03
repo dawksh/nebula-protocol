bcc6509703ccc8a88aef089e25eb0a88c9697305
daksh <akshd@outlook.com>
2024-08-03 23:22:41 +0530

    chore: add test

---
+++ test/Nebula.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Nebula} from "../src/Nebula.sol";
+
+contract NebulaTest is Test {
+    Nebla public nebula;
+
