56ac8984d21bc0181edb5da704cd0550b05e7712
daksh <akshd@outlook.com>
2024-09-02 20:20:11 +0530

    feat: add deploy script

--- script/NebulaClean.s.sol
+++ script/NebulaClean.s.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "../src/NebulaRegistry.sol";
-import {WorldIDVerifier} from "../src/WorldIdVerifier.sol";
+import {Nebula} from "../src/Nebula.sol";
 
-contract MyScript is Script {
+contract NebulaClean is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
+        address eas = address(0);
+        bytes32 easSchema = bytes32(0);
 
         NebulaRegistry registry = new NebulaRegistry();
+
+        Nebula nebula = new Nebula(address(registry), eas, easSchema);
 
         vm.stopBroadcast();
     }
